80d6ccd8efaa575b12d72bfe88d49748f1cbefd7
Dillon Kabot <dkabot177@gmail.com>
2021-06-09 13:59:43 -0500

    Initial Commit

---
+++ data_types.py
@@ -0,0 +1,23 @@
+import os
+os.system('clear')
+
+#String
+first_name = "dillon"
+#number
+age = 25
+#list
+names = ['bob', 'jack', 'jill']
+
+
+# Dictionary
+# 'john' is key, 'pepperoni' is valued pair
+fav_pizza = {
+    "John": "Pepperoni",
+    "Dillon": 'Hawaiian Pizza',
+    "mary": 'Cheese',
+}
+
+print (fav_pizza['Dillon'])
+
+
+#Boolean true or false
---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import os
+os.system('clear')
+
+# This is a comment
+'''
+For a multiline comment
+
+'''
+full_name = "Dillon Kabot"
+
+
+print('hello world')
+print(full_name)
